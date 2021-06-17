e41e643ab272e7afb5bd35e15606a46f3c6f6732
mohdshahbaz123 <52749826+mohdshahbaz123@users.noreply.github.com>
2021-06-17 18:17:12 +0530

    Add files via upload

---
+++ 02_temp_conversion_user_choice.cpp
@@ -0,0 +1,32 @@
+// program 02
+// Temperature conversion from fahrenheit to celsius & vice versa with user choice
+// 17 jun 21
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int choice;
+    double temp, conv_temp;
+
+    cout<<"Enter your option\n";
+    cout<<"1. Conversion from fahrenheit to celsius\n";
+    cout<<"2. Conversion from celsius to fahrenheit\n";
+    cin>>choice;
+
+    if (choice == 1){
+        cout<<"Enter temperature in fahrenheit: ";
+        cin>> temp ;
+        conv_temp = (temp-32)/1.8;
+        cout<<"The temperature in celsius is " << conv_temp <<", for " << temp << " fahrenheit";
+    }
+
+    if (choice == 2){
+        cout<<"Enter temperature in celsius: ";
+        cin>> temp ;
+        conv_temp = 1.8 * temp + 32;
+        cout<<"The temperature in fahrenheit is " << conv_temp <<", for " << temp << " celsius";    
+    }
+    return 0;
+}
